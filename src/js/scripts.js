52f9f08e7b55dd57203cbd9b09ecd075690f1af8
Vyacheslav Sinitsin <sinitsin.yar@gmail.com>
2023-02-10 23:20:17 +0300

    hover on section business and add scrollup

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,2 +1,14 @@
 // Custom Scripts
-@@include('main.js')
+// @@include('main.js')
+
+/*========= SHOW SCROLL UP ==========*/
+const scrollUp = () => {
+	const scrollUp = document.getElementById('scroll-up')
+    
+	if (scrollY >= 350) {
+		scrollUp.classList.add('show-scroll')
+	} else {
+		scrollUp.classList.remove('show-scroll')
+	}
+}
+window.addEventListener('scroll', scrollUp)

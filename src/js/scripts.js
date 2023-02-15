7cecd2ea1004d482adf97e69b547784ea7751db8
Vyacheslav Sinitsin <sinitsin.yar@gmail.com>
2023-02-15 15:14:15 +0300

    add accondion business section

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -90,6 +90,26 @@ class ItcAccordion {
     new ItcAccordion(document.querySelector('.accordion'), {
 			alwaysOpen: true,
 	})
+
+var acc = document.getElementsByClassName('accordion')
+var i
+
+for (i = 0; i < acc.length; i++) {
+	acc[i].addEventListener('click', function () {
+		/* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+		this.classList.toggle('active')
+
+		/* Toggle between hiding and showing the active panel */
+		var panel = this.nextElementSibling
+		if (panel.style.display === 'block') {
+			panel.style.display = 'none'
+		} else {
+			panel.style.display = 'block'
+		}
+	})
+}
+
 // Swiper
 let swiper = new Swiper('.mySwiper', {
 	loop: true,
